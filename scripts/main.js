3a895629adb5f59b029c866f8f332b814ac5bd7e
ZeeQ <kw133875@gmail.com>
2024-12-23 00:38:37 -0600

    rough draft

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,18 @@
 import { world, system } from "@minecraft/server";
 
-// Constants for reach detection
+
 const MAX_SURVIVAL_REACH = 5.5;
 const CHECK_INTERVAL = 10;
 
-// Track the last hit entity for each player
-const lastHits = new Map();
 
-// Listen for entity hit events
+
+
+
 world.afterEvents.entityHitEntity.subscribe((eventData) => {
     const player = eventData.damagingEntity;
     const hitEntity = eventData.hitEntity;
     
-    // Only process if the attacker is a player and there's a hit entity
+    
     if (player.typeId === "minecraft:player" && hitEntity) {
         // Calculate distance between player and hit entity
         const playerPos = player.location;
